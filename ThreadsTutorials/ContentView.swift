2b5a77d2ca6f63a116c68cff450d457aa9b0c114
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-06-20 10:54:39 -0500

    Initial Commit

---
+++ ThreadsTutorials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadsTutorials
+//
+//  Created by usradmin on 20/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadsTutorials/ThreadsTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialsApp.swift
+//  ThreadsTutorials
+//
+//  Created by usradmin on 20/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
