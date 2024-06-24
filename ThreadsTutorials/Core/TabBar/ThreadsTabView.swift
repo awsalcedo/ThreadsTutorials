42f8d86f68ddbe3c22635ffe1470403019b09087
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-06-24 11:28:39 -0500

    Configure TabView

--- ThreadsTutorials/Core/TabBar/ThreadsTabView.swift
+++ ThreadsTutorials/Core/TabBar/ThreadsTabView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 
 struct ThreadsTabView: View {
+    //Para controlar la selección de la pestaña actual
     @State var selectedTab = 0
     var body: some View {
         TabView(selection: $selectedTab) {
             Text("Feed View")
+            //Definimos el item de la pestaña
                 .tabItem {
                     Image(systemName: selectedTab == 0 ? "house.fill" : "house")
                         .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none) //Esto hace que cuando el tab esté seleccionado la imagen se muestre rellena caso contrario la imagen no se muestra rellena
                 }
+            //Cuando la pestaña aparece se marque como seleccionada
+                .onAppear { selectedTab = 0 }
+            //Asigna un valor de etiqueta a esta pestña y se utiliza para gestionar la selección de las pestañas dentro del TabView
+                .tag(0)
+            
+            Text("Explore")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            //Este es distinto al de arriba porque la lupa no tiene una imagen con relleno
+                .onAppear{ selectedTab = 1}
+                .tag(1)
+            
+            Text("Upload Thread")
+                .tabItem {
+                    Image(systemName: "plus")
+                }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
+            
+            Text("Activity")
+                .tabItem {
+                    Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
+                        .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                        .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
+    
         }
+        .tint(.black)
     }
 }
 
