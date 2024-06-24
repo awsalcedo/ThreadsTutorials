a5936e09dee666057a439f5816df5fdd9add2cf8
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-06-24 11:49:48 -0500

    Create views of TabView

---
+++ ThreadsTutorials/Core/ThreadCreation/View/CreateThreadView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateThreadView.swift
+//  ThreadsTutorials
+//
+//  Created by usradmin on 24/6/24.
+//
+
+import SwiftUI
+
+struct CreateThreadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateThreadView()
+}
