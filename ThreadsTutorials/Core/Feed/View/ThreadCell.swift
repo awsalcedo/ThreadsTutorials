0c57c85772cb5f72f26f2f86863f763a7ed203aa
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-06-24 16:35:59 -0500

    FeedView  UI

---
+++ ThreadsTutorials/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadCell.swift
+//  ThreadsTutorials
+//
+//  Created by usradmin on 24/6/24.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
