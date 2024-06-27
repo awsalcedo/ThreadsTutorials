3ba1649d01d533758614aef01ffc4d0becba4e8b
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-06-27 15:42:59 -0500

    EditProfileView

---
+++ ThreadsTutorials/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  ThreadsTutorials
+//
+//  Created by usradmin on 27/6/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
