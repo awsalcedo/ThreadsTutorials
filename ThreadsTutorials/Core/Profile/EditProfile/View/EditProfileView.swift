e52afa5d50e2d405e182603ce9d036d37c7e269b
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-06-27 15:43:42 -0500

    EditProfileView UI

--- ThreadsTutorials/Core/Profile/EditProfile/View/EditProfileView.swift
+++ ThreadsTutorials/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -8,8 +8,97 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    @State var bio = ""
+    @State var link = ""
+    @State var isPrivateProfile = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            /*
+             Para superponer la vista de edición de los botones de la parte superior, también es usado para diseños complejos como por ejemplo colocar un texto sobre una imagen, o un botón en la esquina de una vista o crear efectos visuales como sombras, fondos semi-transparentes o gráficos personalizados
+             */
+            ZStack {
+                Color(.systemGroupedBackground)
+                /* Para especificar que bordes de la vista deben
+                 ignorar las áreas seguras de la pantlla
+                 */
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    // nombre e imagen del profile
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            
+                            Text("Alex Salcedo")
+                        }
+                        
+                        Spacer()
+                        
+                        CircularProfileImageView()
+                    }
+                    
+                    Divider()
+                    
+                    // campos para la biografía
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Enter your bio...", text: $bio, axis: .vertical)
+                        
+                         
+                    }
+                    
+                    Divider()
+                    
+                    // campos para link
+                    VStack(alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Add link...", text: $link)
+                    }
+                    
+                    Divider()
+                    
+                    // activar profile privado
+                    Toggle("Private profile", isOn: $isPrivateProfile)
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                // para darle el borde negro al recuadro
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .padding() // para que le aplique sólo a la vista de recuadro
+                
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Done") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
