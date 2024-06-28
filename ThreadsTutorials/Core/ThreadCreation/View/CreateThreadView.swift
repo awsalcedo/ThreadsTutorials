7c15a45fe42c6f3ddab2c8f1388bda15067c0f99
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-06-28 16:47:09 -0500

    Create Thread View

--- ThreadsTutorials/Core/ThreadCreation/View/CreateThreadView.swift
+++ ThreadsTutorials/Core/ThreadCreation/View/CreateThreadView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    @State var caption = ""
     var body: some View {
-        Text("Hello, Create Thread")
+        /*
+         No hay navegación pero lo usamos para obtener la barra superior de navegación
+         */
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("alex_salcedo")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start a thread...", text: $caption, axis: .vertical)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if caption.isEmpty {
+                        Button {
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1.0)
+                    .disabled(caption.isEmpty)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
