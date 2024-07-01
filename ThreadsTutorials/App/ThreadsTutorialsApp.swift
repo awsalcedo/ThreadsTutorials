add40953d5cbca767ea0a71a8a8ac1cb98c723e7
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-07-01 12:08:28 -0500

    Configure Firebase

--- ThreadsTutorials/App/ThreadsTutorialsApp.swift
+++ ThreadsTutorials/App/ThreadsTutorialsApp.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+// se agrega para configurar Firebase
+import FirebaseCore
+
+/*
+ Se añade todo el AppDelegate
+ */
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ThreadsTutorialsApp: App {
+    // Registra el AppDelegate para la configuración de Firebase
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
