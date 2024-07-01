4f2fbcfb6f1771a0f986cb637f76dd749ffd6013
Alex Salcedo <awsalcedo@sri.gob.ec>
2024-07-01 16:44:58 -0500

    Add comments

--- ThreadsTutorials/Core/Authentication/ViewModel/RegistrationViewModel.swift
+++ ThreadsTutorials/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -6,8 +6,13 @@
 //
 
 import Foundation
-
+/*
+ ObservableObject es un protocolo que permite que la vista observe los cambios en el estado de la clase y de actualice automáticamente cuando dichos cambios ocurren.
+ */
 class RegistrationViewModel: ObservableObject {
+    /*
+     @Published es un atributo de propiedad que SwiftUI observa automáticamente, cuando cambia el valor de email, cualquier vista que esté observando RegistrationViewModel se actualizará automáticamente.
+     */
     @Published var email = ""
     @Published var password = ""
     @Published var fullname = ""
@@ -15,6 +20,9 @@ class RegistrationViewModel: ObservableObject {
     
     @MainActor
     func createUser() async throws {
+        /*
+         Llama a un método asíncrono createUser en un servicio compartido
+         */
         try await AuthService.shared.createUser(
             withEmail: email,
             password: password, 
